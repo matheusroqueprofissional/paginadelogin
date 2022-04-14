581a43aed8952e7ce6fe8fb8073dd12a20190c0c
matheusroqueprofissional <matheus.roqueprofissional@gmail.com>
2022-04-14 20:56:55 -0300

    adicionado dark-mode e botao checkbox

---
+++ index.js
@@ -0,0 +1,42 @@
+function enviar(){
+
+var username = document.getElementById("username");
+var password = document.getElementById("password");
+
+
+if(username.value == "")
+{
+    alert("campo login nao inserido");
+}
+if(password.value == "")
+{
+    alert("campo password não inserido");
+}
+
+
+
+
+
+
+
+
+
+if(username.value == "matheus" && password.value == "1234")
+{
+
+alert("bem vindo mestre");
+
+
+}
+
+
+
+
+
+}
+
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
